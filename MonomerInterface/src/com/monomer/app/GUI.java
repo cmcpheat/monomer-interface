8f3a5d70bd2ab6c12313abc018b0ccfcb67a54c3
Chris McPheat <chrismcpheat@icloud.com>
2020-04-10 18:17:08 +0100

    tidying GUI class

--- MonomerInterface/src/com/monomer/app/GUI.java
+++ MonomerInterface/src/com/monomer/app/GUI.java
@@ -1,62 +1,21 @@
 package com.monomer.app;
 
-import com.monomer.data.DataObject;
 import com.monomer.gui.pages.create_record.CreateRecordPage;
 import com.monomer.gui.pages.live_data.LiveDataPage;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
-import javax.swing.Timer;
-import javax.swing.border.LineBorder;
+
 
 public class GUI {
 	
 	private JFrame frame;
-	private JPanel chartPanel;
-	private JButton machineOneBtn;
-	private JButton machineTwoBtn;
-	private JButton machineThreeBtn;
-	private JPanel formPanel;
-	private JButton submitBtn;
-	private JButton clearBtn;
-	private JLabel batchLabel;
-	private JLabel batchValidationLabel;
-	private JLabel machineLabel;
-	private JLabel machineValidationLabel;
-	private JLabel bubbleLabel;
-	private JLabel bubbleValidationLabel;
-	private JLabel submitMessageLabel;
-	private JTextField batchText;
-	private JComboBox machineText;
-	private JTextField bubbleText;
-	private boolean machineOneActive = false;
-	private boolean machineTwoActive = false;
-	private boolean machineThreeActive = false;
-	private boolean batchIsValidFormat = false;
-	private boolean bubbleIsValidFormat = false;
-	private int batchId;
-	private int machineNumber;
-	private int bubbleCount;
-	private String dateTime;
 
 	// GUI constructor 
 	public GUI() {
