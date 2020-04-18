bb536891b1fc35e8b2d42fa04635353bc21bdfa3
Chris McPheat <chrismcpheat@icloud.com>
2020-04-18 15:24:44 +0100

    serversocket added to main - only one instance of app allowed to run

--- MonomerInterface/src/com/monomer/app/Main.java
+++ MonomerInterface/src/com/monomer/app/Main.java
@@ -5,37 +5,37 @@ import java.net.ServerSocket;
 
 import javax.swing.SwingUtilities;
 
-import com.monomer.controllers.BatchIdController;
 import com.monomer.controllers.GuiController;
 import com.monomer.views.Theme;
 
 public class Main {
 	
 	public static ServerSocket ss;
-	static BatchIdController batchIdController = new BatchIdController();
 	
 	// run the application
 	public static void main(String[] args) {
 		
+		// set theme to custom
+		Theme theme = new Theme();
+		theme.setTheme();
+		
 		ss = null;
 		System.out.println("Starting app... \n");
+		
+		// only one instance of app is allowed to run
 		try {
-			ss = new ServerSocket(1044);			
+			ss = new ServerSocket(1044);
+			
+			// GUI runs on its own thread
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					new GuiController();	
+				}
+			});
 		}
 		catch (IOException io) {
 			System.err.println("Application already running!");
 			System.exit(-1);
 		}
-		
-		// set theme to 'nimbus'
-		Theme theme = new Theme();
-		theme.setTheme();
-		
-		// GUI runs on its own thread
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new GuiController();	
-			}
-		});
 	}
 }	
